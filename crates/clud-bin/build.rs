92e9ff434d327ddc7b8a431bcd79acab3b5c8f55
zackees <z@zackees.com>
2026-06-08 19:01:30 -0700

    build: allow large generated worker oneof

--- crates/clud-bin/build.rs
+++ crates/clud-bin/build.rs
@@ -2,6 +2,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=../../proto/clud_v1.proto");
     println!("cargo:rerun-if-changed=build.rs");
     let file_descriptors = protox::compile(["../../proto/clud_v1.proto"], ["../../proto/"])?;
-    prost_build::compile_fds(file_descriptors)?;
+    let mut config = prost_build::Config::new();
+    config.enum_attribute(
+        ".clud.v1.WorkerServerEnvelope.message",
+        "#[allow(clippy::large_enum_variant)]",
+    );
+    config.compile_fds(file_descriptors)?;
     Ok(())
 }
